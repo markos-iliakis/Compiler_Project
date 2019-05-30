98bb8c0d023bce9165211eb57dc4009051ddfcc6
Markos <markosiliakis@outlook.com>
2019-05-31 00:07:44 +0300

    tests

---
+++ test_files/tests/mytest.java
@@ -0,0 +1,17 @@
+class myTest{
+    public static void main(String[] a){
+
+    }
+}
+
+
+// This class invokes the methods to create a tree,
+// insert, delete and serach for  elements on it
+class My {
+    int x;
+    public int Start(int z) {
+        int y;
+        x = z;
+        return y;
+    }
+}
